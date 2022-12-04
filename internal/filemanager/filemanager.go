a9a8793451d311a356221d7dffb1a77f0e81ecbd
Muhammed Hussein Karimi <info@karimi.dev>
2022-12-04 12:01:29 +0330

    save file moved to batch

--- internal/filemanager/filemanager.go
+++ internal/filemanager/filemanager.go
@@ -40,40 +40,47 @@ func SaveFile(dir, filename string, source io.Reader) error {
 		return err
 	}
 	var chunckID int64 = 0
-	for {
-		dstFileName := fmt.Sprintf("%v__%v", fullFilename, chunckID)
-		if _, err := os.Stat(dstFileName); !os.IsNotExist(err) {
-			return err
-		}
-		dst, err := os.Create(dstFileName)
-		if err != nil {
-			return err
-		}
-		defer dst.Close()
-		written, err := io.CopyN(dst, source, cfg.ChunkSize)
-		if err == io.EOF || err == nil {
-			fileFinished := err == io.EOF
-			err := db.Update(func(tx *bolt.Tx) error {
-				b, err := tx.CreateBucket([]byte(internalFilename))
-				if err == bolt.ErrBucketExists {
-					b = tx.Bucket([]byte(internalFilename))
-				} else if err != nil {
-					return err
-				}
-				return b.Put(typeconverters.Int64ToBytes(chunckID), typeconverters.Int64ToBytes(written))
-			})
+	return db.Batch(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte(internalFilename))
+		if err == bolt.ErrBucketExists {
+			err = tx.DeleteBucket([]byte(internalFilename))
 			if err != nil {
 				return err
 			}
-			if fileFinished {
-				return nil
-			} else {
-				chunckID++
+			b, err = tx.CreateBucket([]byte(internalFilename))
+			if err != nil {
+				return err
 			}
 		} else if err != nil {
 			return err
 		}
-	}
+		for {
+			dstFileName := fmt.Sprintf("%v__%v", fullFilename, chunckID)
+			if _, err := os.Stat(dstFileName); !os.IsNotExist(err) {
+				return err
+			}
+			dst, err := os.Create(dstFileName)
+			if err != nil {
+				return err
+			}
+			defer dst.Close()
+			written, err := io.CopyN(dst, source, cfg.ChunkSize)
+			if err == io.EOF || err == nil {
+				fileFinished := err == io.EOF
+				err = b.Put(typeconverters.Int64ToBytes(chunckID), typeconverters.Int64ToBytes(written))
+				if err != nil {
+					return err
+				}
+				if fileFinished {
+					return nil
+				} else {
+					chunckID++
+				}
+			} else if err != nil {
+				return err
+			}
+		}
+	})
 }
 
 func RemoveFile(dir, filename string) error {
